c631fd3e0e5dcd6f5e2e527c1ec5cfe626d2eeb9
Phil McGarty <philmcgarty@hotmail.com>
2022-04-20 21:59:30 -0400

    added enemy object

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,10 +1,11 @@
 const Player = require('../lib/Player.js');
 const Potion = require('../lib/Potion.js');
+
 jest.mock('../lib/Potion');
-console.log(new Potion());
+//console.log(new Potion());
 
 test('creates a player object', () => {
-    const player =new Player('Dave');
+    const player = new Player('Dave');
 
     expect(player.name).toBe('Dave');
     expect(player.health).toEqual(expect.any(Number));
